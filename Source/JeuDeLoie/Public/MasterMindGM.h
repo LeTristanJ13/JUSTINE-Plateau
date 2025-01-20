e42d58891a2f1e99316a49e42ab68e9b8d0ddbfc
Justine Tristan <justine.tristan@yahoo.com>
2025-01-20 16:58:44 +0100

    update

---
+++ Source/JeuDeLoie/Public/MasterMindGM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MasterMindGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JEUDELOIE_API AMasterMindGM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/JeuDeLoie/Public/MyBlackboardKeyType_Enum.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Enum.h"
+#include "MyBlackboardKeyType_Enum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JEUDELOIE_API UMyBlackboardKeyType_Enum : public UBlackboardKeyType_Enum
+{
+	GENERATED_BODY()
+	
+};
